295614e993a5cdfb245fec3eee8233013a319f19
Dimasariza <roganda.sihombing11@gmail.com>
2023-04-03 19:32:37 +0700

    change greating message to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Greating from '@/component/greating'
 
-// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,9 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1> 
+        <Greating />
       </main>
     </>
   )
